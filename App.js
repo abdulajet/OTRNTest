ddf1e935ae8e6e5bdb46d07f952dd059be817738
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2021-04-13 18:36:23 +0100

    init

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { OTSession, OTPublisher, OTSubscriber } from 'opentok-react-native';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.apiKey = '';
+    this.sessionId = '';
+  }
+  render() {
+    return (
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <OTSession apiKey={this.apiKey} sessionId={this.sessionId} token={this.token}>
+          <OTPublisher style={{ width: 100, height: 100 }} />
+          <OTSubscriber style={{ width: 100, height: 100 }} />
+        </OTSession>
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ ios/Test.swift
@@ -0,0 +1,8 @@
+//
+//  Test.swift
+//  OTTest
+//
+//  Created by Abdulhakim Ajetunmobi on 13/04/2021.
+//
+
+import Foundation
